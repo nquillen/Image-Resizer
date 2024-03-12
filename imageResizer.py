0593c5b3b5c5e2193a2c3871361ff3aeaef7ce8e
Nick <nicktquillen@gmail.com>
2024-03-12 15:53:30 -0400

    Creates output folder if none, added default output

--- imageResizer.py
+++ imageResizer.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
+from pathlib import Path
 
     
 def resizeImage(image, WIDTH, HEIGHT, outputFolder):
-    filename, fileExtension = os.path.splitext(image) # splits the extension from the filename for easier saving and printing
-    # print(filename)
-    # print(fileExtension)
+    path, filename = os.path.split(image)
+    filename, fileExtension = os.path.splitext(filename) # splits the extension from the filename for easier saving and printing
 
     try: 
         image = Image.open(image) # Open the image, causes an exception if file is not an image
@@ -17,20 +17,22 @@ def resizeImage(image, WIDTH, HEIGHT, outputFolder):
     except IOError: # Exceptions occur when not image file
         print("Skipping non-image file: " + filename)
 
-
+# Get input for file paths and desired image size
 print("Path of folder of images to resize:", end="")
 path = input()
 print("Width of image desired:", end = "")
 width = int(input())
 print("Height of image desired:", end = "")
 height = int(input())
-print("Desired output folder:", end = "")
+print("Desired output folder (leave blank to save in same folder):", end = "")
 outputFolder = input()
 print()
 
 entries = os.scandir(path)
 for entry in entries:
-    
+    if not outputFolder:
+        outputFolder = path
+    Path(outputFolder).mkdir(parents=True, exist_ok=True) # Make directory if it doesn't exist
     resizeImage((path + "\\" + entry.name), width, height, outputFolder)
     
 
